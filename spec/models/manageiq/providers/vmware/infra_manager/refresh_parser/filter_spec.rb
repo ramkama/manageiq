0fadf2e6e986b7e00569860fcd822b2cc22ae5c3
Adam Grare <agrare@redhat.com>
2016-11-14 12:22:28 -0500

    Add simple two datacenter test for folder refresh

--- spec/models/manageiq/providers/vmware/infra_manager/refresh_parser/filter_spec.rb
+++ spec/models/manageiq/providers/vmware/infra_manager/refresh_parser/filter_spec.rb
@@ -85,5 +85,56 @@ describe ManageIQ::Providers::Vmware::InfraManager::RefreshParser::Filter do
         end
       end
     end
+
+    context "with two datacenters" do
+      let(:dc1)         { FactoryGirl.create(:datacenter, :ems_ref => "datacenter-1", :ems_ref_obj => "datacenter-1", :name => "dc1") }
+      let(:dc2)         { FactoryGirl.create(:datacenter, :ems_ref => "datacenter-2", :name => "dc2") }
+      let(:root_folder) { FactoryGirl.create(:ems_folder, :ems_ref => "group-d1",     :name => "Datacenters") }
+      let(:vm_folder_1) { FactoryGirl.create(:ems_folder, :ems_ref => "group-v3",     :name => "vm") }
+      let(:vm_folder_2) { FactoryGirl.create(:ems_folder, :ems_ref => "group-v4",     :name => "vm") }
+      let(:vc_data) do
+        inv = Hash.new { |h, k| h[k] = {} }
+
+        [[dc1.ems_ref, vm_folder_1.ems_ref], [dc2.ems_ref, vm_folder_2.ems_ref]].each do |dc_ref, folder_ref|
+          inv[:dc][dc_ref] = {
+            "MOR"      => dc_ref,
+            "parent"   => root_folder.ems_ref,
+            "vmFolder" => folder_ref
+          }
+        end
+
+        inv[:folder][root_folder.ems_ref] = {
+          "MOR"         => root_folder.ems_ref,
+          "childEntity" => [dc1.ems_ref, dc2.ems_ref]
+        }
+
+        [[vm_folder_1.ems_ref, dc1.ems_ref], [vm_folder_2.ems_ref, dc2.ems_ref]].each do |folder_ref, dc_ref|
+          inv[:folder][folder_ref] = {
+            "MOR"    => folder_ref,
+            "parent" => dc_ref
+          }
+        end
+
+        inv
+      end
+
+      context "targeting a datacenter" do
+        it "returns the root folder" do
+          filtered_data = @refresher.filter_vc_data(ems, dc1)
+          expect(filtered_data[:folder]).to include(root_folder.ems_ref)
+        end
+
+        it "doesn't return the other datacenter" do
+          filtered_data = @refresher.filter_vc_data(ems, dc1)
+          expect(filtered_data[:dc]).not_to include(dc2.ems_ref)
+        end
+
+        it "includes the correct child folders" do
+          filtered_data = @refresher.filter_vc_data(ems, dc1)
+          expect(filtered_data[:folder]).to     include(vm_folder_1.ems_ref)
+          expect(filtered_data[:folder]).not_to include(vm_folder_2.ems_ref)
+        end
+      end
+    end
   end
 end
